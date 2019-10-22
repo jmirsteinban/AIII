df0d9d726f973cc3f2b98ece3e1a48fcfb1d3d11
Diego Padilla <dpadillam1@hotmail.com>
2019-10-22 06:46:21 -0600

    se agrega proforma, se toma como base factura, se agrega sweetalerts para mostrar alertas con formato, jquery.maskmoney para número, select2 para búsqueda filtrada en selects, se ocultas las columnas de la tabla cedula cliente, cedula usuario, estado factura.

---
+++ Proyecto/HRN-CODE/FrontEnd/Controllers/ProformaController.cs
@@ -0,0 +1,57 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace FrontEnd.Controllers
+{
+    public class ProformaController : Controller
+    {
+        // GET: Proforma
+        public ActionResult Create()
+        {
+            LlenarLista();
+            ViewBag.CedulaUser = Session["cedula"];
+
+            return View();
+        }
+
+        private void LlenarLista()
+        {
+            List<product> Productos;
+
+            using (WorkUnit<product> unidad = new WorkUnit<product>(new BDContext()))
+            {
+                Productos = unidad.genericDAL.GetAll().ToList();
+            }
+
+            List<Producto> products = new List<Producto>();
+
+            foreach (var item in Productos)
+            {
+                Producto producto = new Producto
+                {
+
+                    productName = item.nombre_producto,
+                    precio_factura_d = item.precio_venta,
+                    cantProd = item.cantidad
+                };
+
+                products.Add(producto);
+            }
+
+            List<Estado> estados = new List<Estado>();
+            estados.Insert(0, new Estado { nombreEstado = "Completado" });
+            estados.Insert(1, new Estado { nombreEstado = "Procesando" });
+
+            ViewBag.Productos = products;
+            ViewBag.Estados = estados;
+        }
+    }
+}
