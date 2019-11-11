0d9a8276203c0dda45004756232e3816b3aad14a
Diego Padilla <dpadillam1@hotmail.com>
2019-11-11 09:07:04 -0600

    cambios en proforma, se agregan keys en el appsetting para configuración de rutas y del correo de envío de proformas.

--- Proyecto/HRN-CODE/FrontEnd/Controllers/ProformaController.cs
+++ Proyecto/HRN-CODE/FrontEnd/Controllers/ProformaController.cs
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-//using IronPdf;
+using IronPdf;
 using System.Net.Mail;
+using System.Configuration;
 
 namespace FrontEnd.Controllers
 {
@@ -47,60 +48,97 @@ namespace FrontEnd.Controllers
                 products.Add(producto);
             }
 
+            List<client> listaClientes;
+            using (WorkUnit<client> workUnit = new WorkUnit<client>(new BDContext()))
+            {
+                listaClientes = workUnit.genericDAL.GetAll().Where(x => x.estado_cliente == "Activo").ToList();
+            }
+
             List<Estado> estados = new List<Estado>();
             estados.Insert(0, new Estado { nombreEstado = "Completado" });
             estados.Insert(1, new Estado { nombreEstado = "Procesando" });
 
             ViewBag.Productos = products;
+            ViewBag.Clientes = listaClientes;
             ViewBag.Estados = estados;
         }
 
 
-        private void enviar_correo(Producto[] order)
+        private string enviar_correo(Producto[] order)
         {
-            string plantilla_html = @"C:\Users\dpadilla\Downloads\pdf\proforma.html";
-            string plantilla_comodines = System.IO.File.ReadAllText(plantilla_html);
-            Random rnd = new Random();
-            int numero = rnd.Next(1, 1000);
 
-            var html_tabla = "";
-            foreach (var product in order)
+            List<client> listaClientes;
+            using (WorkUnit<client> workUnit = new WorkUnit<client>(new BDContext()))
             {
-                html_tabla += "<tr>";
-                html_tabla += "<td>" + product.cantProd + "</td>";
-                html_tabla += "<td>" +product.productName +"</td>";
-                html_tabla += "<td>" + "1000" + "</td>";
-                html_tabla += "<td>" + product.cantProd  * 1000 + "</td>";
-                html_tabla += "</tr>";
-
+                listaClientes = workUnit.genericDAL.GetAll().Where(x => x.cedula_cliente == order[0].cedCliente).ToList();
             }
-            plantilla_comodines = plantilla_comodines.Replace("@tabla", html_tabla);
-            plantilla_comodines = plantilla_comodines.Replace("@proforma", "PROFORMA-"+numero.ToString());
-            plantilla_comodines = plantilla_comodines.Replace("@Cliente", order[0].cedCliente);
-            plantilla_comodines = plantilla_comodines.Replace("@usuario", order[0].cedUsuario);
-            //IronPdf.HtmlToPdf Renderer = new IronPdf.HtmlToPdf();
-            //Renderer.PrintOptions.CreatePdfFormsFromHtml = true;
-            string html_cuerpo = "";
-            html_cuerpo = "<html>";
-            html_cuerpo += "<body>";
-            html_cuerpo += "<p>Estimado cliente:</p><br>";
-            html_cuerpo += "<p>Adjunto encontrará la factura proforma solicitada.</p><br>";
-            html_cuerpo += "<p>Saludos,</p>";
-            html_cuerpo += "</body>";
-            html_cuerpo += "</html>";
-            //Renderer.RenderHtmlAsPdf(plantilla_comodines).SaveAs(@"C:\Users\dpadilla\Downloads\pdf\proforma"+numero.ToString()+".pdf");
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.live.com");
-            mail.From = new MailAddress("diego.padilla.miranda@hotmail.com");
-            mail.To.Add("dpadillam1@hotmail.com");
-            mail.Subject = "Nueva Proforma - " + numero.ToString();
-            mail.IsBodyHtml = true;
-            mail.Body = html_cuerpo;
-            mail.Attachments.Add(new Attachment(@"C:\Users\dpadilla\Downloads\pdf\proforma" + numero.ToString() + ".pdf"));
-            SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("diego.padilla.miranda@hotmail.com", "0ym754rbn58");
-            SmtpServer.EnableSsl = true;
-            SmtpServer.Send(mail);
+            if(listaClientes.Count != 0)
+            {
+                try
+                {
+                    /*Inicio de appsettings*/
+                    string rutaPDF = System.Configuration.ConfigurationManager.AppSettings["RutaGeneradorPDF"].ToString();
+                    string rutaHTML = System.Configuration.ConfigurationManager.AppSettings["RutaPlantillaHTML"].ToString();
+                    string plantilla_html =  rutaHTML + "proforma.html";
+                    string plantilla_comodines = System.IO.File.ReadAllText(plantilla_html);
+                    string correo_notificacion = System.Configuration.ConfigurationManager.AppSettings["Correo_Notificacion"].ToString();
+                    string pass_correo = System.Configuration.ConfigurationManager.AppSettings["Pass_Notificacion"].ToString();
+                    /*cierre de appsetings*/
+
+                    Random rnd = new Random();
+                    int numero = rnd.Next(1, 1000);
+                    var html_tabla = "";
+                    foreach (var product in order)
+                    {
+                        html_tabla += "<tr>";
+                        html_tabla += "<td>" + product.cantProd + "</td>";
+                        html_tabla += "<td>" + product.productName + "</td>";
+                        html_tabla += "<td>" + "1000" + "</td>";
+                        html_tabla += "<td>" + product.cantProd * 1000 + "</td>";
+                        html_tabla += "</tr>";
+
+                    }
+                    plantilla_comodines = plantilla_comodines.Replace("@tabla", html_tabla);
+                    plantilla_comodines = plantilla_comodines.Replace("@proforma", "PROFORMA-" + numero.ToString());
+                    plantilla_comodines = plantilla_comodines.Replace("@Cliente", order[0].cedCliente);
+                    plantilla_comodines = plantilla_comodines.Replace("@usuario", order[0].cedUsuario);
+                    IronPdf.HtmlToPdf Renderer = new IronPdf.HtmlToPdf();
+                    Renderer.PrintOptions.CreatePdfFormsFromHtml = true;
+                    string html_cuerpo = "";
+                    html_cuerpo = "<html>";
+                    html_cuerpo += "<body>";
+                    html_cuerpo += "<p>Estimado (a) cliente: " + listaClientes[0].primer_nombre_cliente + " " + listaClientes[0].primer_apellido_cliente + "</p><br>";
+                    html_cuerpo += "<p>Adjunto encontrará la factura proforma solicitada.</p><br>";
+                    html_cuerpo += "<p>Saludos,</p>";
+                    html_cuerpo += "</body>";
+                    html_cuerpo += "</html>";
+                    Renderer.RenderHtmlAsPdf(plantilla_comodines).SaveAs(rutaPDF + "proforma" + numero.ToString() + ".pdf");
+                    MailMessage mail = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp.live.com");
+                    mail.From = new MailAddress("diego.padilla.miranda@hotmail.com");
+                    mail.To.Add(listaClientes[0].correo_electronico_cliente);
+                    mail.CC.Add("dpadillam1@hotmail.com");
+                    mail.Subject = "Nueva Proforma - " + numero.ToString();
+                    mail.IsBodyHtml = true;
+                    mail.Body = html_cuerpo;
+                    mail.Attachments.Add(new Attachment(rutaPDF+  "proforma" + numero.ToString() + ".pdf"));
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential(correo_notificacion, pass_correo);
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(mail);
+                    return "1";
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+                
+            }
+            else
+            {
+                return "2";
+            }
+            
         }
 
 
@@ -109,50 +147,8 @@ namespace FrontEnd.Controllers
         {
             try
             {
-                enviar_correo(order);
-                /*string cedula_cliente = order[0].cedCliente, cedula_user = order[0].cedUsuario,
-                       estado = order[0].estado;
-
-                sale Factura;
-                sales_x_products FactDetalle;
-
-                using (WorkUnit<sale> unit = new WorkUnit<sale>(new BDContext()))
-                {
-                    Factura = ConvertirMaestroCreate(cedula_cliente, cedula_user, estado);
-                    decimal totalFactura = 0;
-                    Factura.monto_total = totalFactura;
-
-                    unit.genericDAL.Add(Factura);
-                    unit.Complete();
-
-                    foreach (var item in order)
-                    {
-                        using (WorkUnit<sales_x_products> unit2 = new WorkUnit<sales_x_products>(new BDContext()))
-                        {
-                            FactDetalle = ConvertirDetalle(Factura, item.productName, Convert.ToInt32(item.cantProd));
-                            totalFactura = totalFactura + (FactDetalle.precio_factura_d * FactDetalle.cantidad);
-                            unit2.genericDAL.Add(FactDetalle);
-                            unit2.Complete();
-                        }
-                    }
-
-                    Factura.monto_total = totalFactura;
-                    unit.genericDAL.Update(Factura);
-                    unit.Complete();
-                }
-
-                client Cliente;
-
-                using (WorkUnit<client> unit = new WorkUnit<client>(new BDContext()))
-                {
-                    Cliente = unit.genericDAL.Get(Factura.clientID);
-
-                }
-
-                return Json(Cliente.primer_nombre_cliente + " " +
-                        Cliente.primer_apellido_cliente + " " + Cliente.segundo_apellido_cliente,
-                        JsonRequestBehavior.AllowGet);*/
-                return Json("1",
+                string respuesta = enviar_correo(order);
+                return Json(respuesta,
                 JsonRequestBehavior.AllowGet);
             }
             catch (Exception msj)
