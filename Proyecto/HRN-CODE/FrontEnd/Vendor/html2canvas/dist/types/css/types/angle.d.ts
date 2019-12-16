32d3fd0cdadd6d1d840606df24dc89514d1bbfb1
Jordan Molina <jmirsteinban@gmail.com>
2019-12-16 07:53:14 -0600

    Publicar

--- Proyecto/HRN-CODE/FrontEnd/Vendor/html2canvas/dist/types/css/types/angle.d.ts
+++ Proyecto/HRN-CODE/FrontEnd/Vendor/html2canvas/dist/types/css/types/angle.d.ts
@@ -1,7 +1,6 @@
 import { CSSValue } from '../syntax/parser';
 import { ITypeDescriptor } from '../ITypeDescriptor';
-import { GradientCorner } from './image';
 export declare const angle: ITypeDescriptor<number>;
 export declare const isAngle: (value: CSSValue) => boolean;
-export declare const parseNamedSide: (tokens: CSSValue[]) => number | GradientCorner;
+export declare const parseNamedSide: (tokens: CSSValue[]) => number | [import("../syntax/tokenizer").NumberValueToken | import("../syntax/tokenizer").DimensionToken, import("../syntax/tokenizer").NumberValueToken | import("../syntax/tokenizer").DimensionToken];
 export declare const deg: (deg: number) => number;
